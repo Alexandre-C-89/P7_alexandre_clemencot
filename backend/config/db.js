d0e60a19312916f055fe80c5d1a34cb3627e99a4
Alexandre <clemencot.alexandre@gmail.com>
2021-11-18 10:58:27 +0100

    ajout sequelize dans le fichier db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const sequelize = require("sequelize");
 
 const db = mysql.createConnection({
 
@@ -8,4 +9,18 @@ const db = mysql.createConnection({
  
     password: ""
  
-  });
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+   db.query("CREATE DATABASE mabdd", function (err, result) {
+        if (err) throw err;
+        console.log("Base de données créée !");
+    });
+});
+
+const sequelize = new Sequelize("nom_base_de_donnees", "root", "", {
+    dialect: "mysql",
+    host: "localhost"
+});
