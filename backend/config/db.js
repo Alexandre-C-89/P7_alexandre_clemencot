c3bd66ff7f0b780f423026bf4f158f5b531b7704
Alexandre <clemencot.alexandre@gmail.com>
2021-11-18 11:03:57 +0100

    ajout try et catch dans db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -24,3 +24,13 @@ const sequelize = new Sequelize("nom_base_de_donnees", "root", "", {
     dialect: "mysql",
     host: "localhost"
 });
+
+try {
+    sequelize.authenticate();
+    console.log('Connecté à la base de données MySQL!');
+    sequelize.query("SELECT eleves.id as 'eleve_id', eleves.nom as 'eleve_nom', eleves.cours_id, cours.nom as 'cours_nom', cours.date as 'cours_date' FROM eleves JOIN cours on eleves.cours_id = cours.id").then(([results, metadata]) => {
+        console.log(results);
+      })
+  } catch (error) {
+    console.error('Impossible de se connecter, erreur suivante :', error);
+}
