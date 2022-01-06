2cd9295f503ac34a36d11516c7c1d347a48b3e45
Alexandre <clemencot.alexandre@gmail.com>
2022-01-06 17:18:08 +0100

    creation de la partie login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -13,7 +13,7 @@ const sequelize = require("../dbConnect");
 exports.signup = (req, res, next) => {
     // J'appel la fonction de hashage de bcrypt
     // const hashPassword = bcrypt.hash(req.body.password, 10) // Je sale le mot de passe 10 fois
-    console.log(req.body);
+    // console.log(req.body);
     User.create({ // Je crée mon utilisateur
         firstname: req.body.firstname,
         lastname: req.body.lastname,
@@ -27,27 +27,22 @@ exports.signup = (req, res, next) => {
 
 // User.destroy
 // Partie connexion de l'utilisateur
-exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email }) // Je compare l'email de la requête avec celui enregistré
-        .then(user => { 
-            if(!user) { // Si l'email ne correspond pas cela renvoie une erreur
-                return res.status(401).json({ error: "Utilisateur non trouvé !"});
+exports.login = async (req, res, next) => {
+    const user = await User.findOne({ where: { email: req.body.email } }) // Je cherche l'email de la requête avec celui enregistré
+        if (!user) {
+            console.log("Utilisateur non trouvé !");
+        } else {
+            const comparePassword = bcrypt.compare(req.body.password, user.password)
+            if (!comparePassword) { // Si ce n'est pas valide je retourne une erreur
+                return res.status(401).json({ error: "Mot de passe incorrect !"});
             }
-            bcrypt.compare(req.body.password, user.passord) // Je compare les deux mots de passes 
-                .then(valid => {
-                    if (!valid) { // Si ce n'est pas valide je retourne une erreur
-                        return res.status(401).json({ error: "Mot de passe incorrect !"});
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign( // J'utilise la méthode sign de JWT pour encodé l'id 
-                            { userId: user._id }, // clé de l'id de l'utilisateur
-                            "RANDOM_TOKEN_SECRET",
-                            { expiresIn: "12h"}, // la clé expire dans 12h
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+            res.status(200).json({
+                user,
+                token: jwt.sign( // J'utilise la méthode sign de JWT pour encodé l'id 
+                    { userId: user.id }, // clé de l'id de l'utilisateur
+                    "RANDOM_TOKEN_SECRET",
+                    { expiresIn: "12h"}, // la clé expire dans 12h
+                )
+            });
+        }
 };
