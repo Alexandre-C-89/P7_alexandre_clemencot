3f01b670a66392ca3cd78c0606e06d68530f25d1
Alexandre <clemencot.alexandre@gmail.com>
2022-01-06 10:12:48 +0100

    creation de auth.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,14 @@
 const express = require("express");
 // j'utilise la fonction router de express 
 const router = express.Router();
+// J'importe mon fichier authentification
+const auth = require("../middleware/auth");
 
 // J'importe mon fichier user
 const userCtrl = require("../controllers/user");
 
 // Ici je créer mes routes pour s'enregistré et ce connecté
-router.post("/signup", userCtrl.signup);
+router.post("/signup", auth, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
